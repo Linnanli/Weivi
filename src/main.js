bc05229b010c6afe46d85860e34f2f5b42d6dcb4
nally.lin <nally.lin@qq.com>
2020-07-10 16:48:01 +0800

    fix: 修改button-group文件夹名称

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import router from './router'
 import GButton from './components/button'
 import './assets/style/index.scss'
 import GIcon from './components/icon'
-import GButtonGroup from './components/button-group.vue'
+import GButtonGroup from './components/button-group'
+import GInput from './components/input'
 
 Vue.config.productionTip = false
 
 Vue.use(GButton)
 Vue.use(GIcon)
 Vue.use(GButtonGroup)
+Vue.use(GInput)
 
 new Vue({
   router,
