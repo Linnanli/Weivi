feb1d8326982e392007fc2b59a86f23b9d5d100b
nally.lin <nally.lin@qq.com>
2020-07-11 23:15:03 +0800

    chore: 抽取scss样式

--- src/components/button-group/button-group.vue
+++ src/components/button-group/button-group.vue
@@ -8,24 +8,3 @@ export default {
   name: 'GButtonGroup'
 }
 </script>
-<style lang="scss" scoped>
-.g-button-group{
-  .g-button{
-    border-radius: 0;
-
-    &:first-of-type{
-      border-radius: var(--border-radius) 0 0 var(--border-radius);
-    }
-    &:last-of-type{
-      border-radius: 0 var(--border-radius) var(--border-radius) 0;
-    }
-    &:not(:first-of-type){
-      margin-left: -1px;
-    }
-    &:hover{
-      position: relative;
-      z-index: 1;
-    }
-  }
-}
-</style>
