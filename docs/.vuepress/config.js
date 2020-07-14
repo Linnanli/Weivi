b8d3c76fcffbe3a0bd03f766083ff2bdb176a957
nally.lin <nally.lin@qq.com>
2020-07-14 14:43:31 +0800

    feat: 新增vuepress插件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Hello, my friend!',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/favicon.ico`
+        }]
+    ],
+    dest: './docs/.vuepress/dist',
+    ga: '',
+    evergreen: true,
+    themeConfig: {
+      sidebarDepth: 2,
+      sidebar: [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: ['/guide/']
+        }
+      ],
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' }
+      ]
+    }
+}
---
+++ docs/guide/README.md
@@ -0,0 +1,10 @@
+## This is guide
+content...
+
+### title1
+content...
+
+### title2
+
+## small title
+content...
