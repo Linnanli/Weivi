3a8c9f08dbcf740e7b1d0e865282bdb10f2b8fb4
nally.lin <nally.lin@qq.com>
2020-07-14 23:34:27 +0800

    chore: 修改展示页面文件名称

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Button from '../views/Button.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Button',
+    component: Button
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/input',
+    name: 'Input',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Input.vue')
   }
 ]
 
---
+++ src/views/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <div>
+      <g-input v-model="inputValue" clearable/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      inputValue: '1'
+    }
+  }
+}
+</script>
