0a4f67e37093ed9acb5d2d40a173843305e2c4e5
nally.lin <nally.lin@qq.com>
2020-07-14 23:40:06 +0800

    feat: 新增input组件

--- src/components/input/input.vue
+++ src/components/input/input.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>
-    1
+  <div class="g-input"
+       :class="{ 'g-input--suffix': showSuffix }">
+    <input class="g-input__inner"
+           type="text"
+           ref="input"
+           :readonly="readonly"
+           :disabled="disabled"
+           @input="handleInput">
+    <div class="g-input__suffix" v-if="showSuffix">
+      <g-icon class="g-input__icon g-input__clear"
+              name="clear"
+              v-if="clearable"
+              @click.native="handleClear"/>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'GInput',
+  model: {
+    props: 'value',
+    event: 'input'
+  },
   props: {
-    name: {
+    value: {
       type: String,
-      required: true
+      default: ''
+    },
+    clearable: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    showSuffix () {
+      return this.clearable
+    }
+  },
+  mounted () {
+    this.inputEle = this.$refs.input
+    this.setInputValue(this.value)
+  },
+  methods: {
+    setInputValue (value) {
+      this.inputEle.value = value
+    },
+    handleClear () {
+      this.setInputValue('')
+      this.handleInput()
+    },
+    handleInput () {
+      this.$emit('input', this.inputEle.value)
     }
   }
 }
