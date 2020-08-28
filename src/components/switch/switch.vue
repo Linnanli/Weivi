bae9bb61cca53b1b206ded43a859971286e211da
nally.lin <nally.lin@dbappsecurity.com.cn>
2020-08-28 23:35:48 +0800

    feat: 新增自定义值和自定义文本

--- src/components/switch/switch.vue
+++ src/components/switch/switch.vue
@@ -8,7 +8,11 @@
            ref="switch"
            :disabled="disabled"
            @change="handleChange"/>
-    <span class="w-switch__overlay"/>
+    <span class="w-switch__overlay">
+      <span class="w-switch__text" v-if="activeText && inactiveText">
+        {{ checked ? activeText : inactiveText }}
+      </span>
+    </span>
   </label>
 </template>
 <script>
@@ -23,31 +27,47 @@ export default {
       type: [String, Number, Boolean],
       default: null
     },
-    label: {
-      type: [String, Number, Boolean]
-    },
-    trueValue: {
+    activeValue: {
       type: [String, Number, Boolean],
       default: true
     },
-    falseValue: {
+    inactiveValue: {
       type: [String, Number, Boolean],
       default: false
     },
-    checked: {
-      type: Boolean,
-      default: false
+    activeText: {
+      type: String,
+      default: null
+    },
+    inactiveText: {
+      type: String,
+      default: null
     },
     disabled: {
       type: Boolean,
       default: false
     }
   },
+  data () {
+    return {
+      checked: false
+    }
+  },
+  mounted () {
+    const checked = this.value === this.activeValue
+    this.setChecked(checked)
+    this.setValue(checked)
+  },
   methods: {
+    setChecked (checked) {
+      this.$refs.switch.checked = checked
+    },
     handleChange (e) {
       this.setValue(e.target.checked)
     },
-    setValue (value) {
+    setValue (checked) {
+      this.checked = checked
+      const value = checked ? this.activeValue : this.inactiveValue
       this.$emit('change', value)
     }
   }
